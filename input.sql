41d0f38b681153485a9ec68c098cf01ae8d71a88
achmadjae06 <47482954+achmadjae06@users.noreply.github.com>
2019-02-19 08:51:57 +0700

    Delete input.sql

--- input.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO mahasiswa (nim, nama, kota, jenis_kelamin, tanggal_lahir)
-VALUES ('11223344', 'Ari Santoso', 'Bekasi', 'Laki-laki', '1998-10-12'), 
-('11223345', 'Ario Talib', 'Cikarang', 'Laki-laki', '1999-11-18'), 
-('11223346', 'Dina Marlina', 'Karawang', 'Perempuan', '1997-12-01'), 
-('11223347', 'Lisa Ayu', 'Bekasi', 'Perempuan', '1996-01-02'), 
-('11223348', 'Tiara Wahidah', 'Bekasi', 'Perempuan', '1980-02-05'), 
-('11223349', 'Anton Sinaga', 'Cikarang', 'Laki-laki', '1998-01-10');
